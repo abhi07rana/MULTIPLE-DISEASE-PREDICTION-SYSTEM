d96b7e2b5cebd8c0dd663316a0c7f50a391d6946
abhi07rana <91740844+abhi07rana@users.noreply.github.com>
2024-03-19 19:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import pickle
 import re
 import pandas as pd
 # import pyttsx3
-from sklearn import preprocessing
-from sklearn.tree import DecisionTreeClassifier, _tree
+#from sklearn import preprocessing
+#from sklearn.tree import DecisionTreeClassifier, _tree
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.svm import SVC
+#from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import cross_val_score
+#from sklearn.svm import SVC
 import csv
 import warnings
 from sklearn.ensemble import RandomForestClassifier
